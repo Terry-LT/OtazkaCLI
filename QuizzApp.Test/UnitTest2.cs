7b09d30fd77860c6c190d550f6938c73194a6fde
Terry-LT <tlatfulla@gmail.com>
2026-03-13 14:16:10 +0100

    Add unit test for CalculateScore

---
+++ QuizzApp.Test/UnitTest2.cs
@@ -0,0 +1,46 @@
+﻿using QuizzApp.Services;
+
+namespace QuizzApp.Test
+{
+    public class ScoreServiceTests
+    {
+        private ScoreService scoreService;
+        public ScoreServiceTests()
+        {
+            scoreService = new ScoreService();
+        }
+        [Fact]
+        public void CalculateScore_ShouldReturnZero_IfQuestionsCountEqualZero()
+        {
+            Assert.Equal(0, scoreService.CalculateScore(0));
+        }
+        [Fact]
+        public void CalculateScore_ShouldReturnZero_IfQuestionsCountLessZero()
+        {
+            Assert.Equal(0, scoreService.CalculateScore(-1));
+        }
+        [Fact]
+        public void CalculateScore_ShouldReturnRightPercent()
+        {
+            scoreService.IncrementScore();
+            scoreService.IncrementScore();
+            scoreService.IncrementScore();
+            Assert.Equal(60, scoreService.CalculateScore(5));
+        }
+        [Fact]
+        public void IncrementScore_ShouldIncreaseScoreByOne()
+        {
+            int originalScore = scoreService.GetScore();
+            scoreService.IncrementScore();
+            Assert.Equal(originalScore+1, scoreService.GetScore());
+        }
+        [Fact]
+        public void IncrementScore_ShouldIncreaseScoreByTwo()
+        {
+            int originalScore = scoreService.GetScore();
+            scoreService.IncrementScore();
+            scoreService.IncrementScore();
+            Assert.Equal(originalScore + 2, scoreService.GetScore());
+        }
+    }
+}
--- QuizzApp.Test/obj/Debug/net8.0-windows/QuizzApp.Test.AssemblyInfo.cs
+++ QuizzApp.Test/obj/Debug/net8.0-windows/QuizzApp.Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuizzApp.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5398ef0a593acc5e422d48a3bceb79a67718eff")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuizzApp.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuizzApp.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
